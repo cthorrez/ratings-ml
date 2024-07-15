333f4e6adf81704556b4f4d99d680d40a7854b65
cthorrez <claytonthorrez@gmail.com>
2024-07-14 22:27:12 -0700

    migrate to use cdfs and add scale

--- gradient_descent.py
+++ gradient_descent.py
@@ -7,91 +7,44 @@ from riix.models.elo import Elo
 from riix.eval import evaluate
 from data_utils import load_dataset
 
-@jax.jit
-def logistic_predict(ratings, matchups):
-    matchup_ratings = ratings[matchups]
-    neg_rating_diffs = matchup_ratings[:, 1] - matchup_ratings[:, 0]
-    # probs = jax.nn.sigmoid(neg_rating_diffs)
-    probs = logistic.cdf(neg_rating_diffs)
-    return probs
-
-@jax.jit
-def logistic_likelihood(ratings, matchups, outcomes):
-    matchup_ratings = ratings[matchups]
-    neg_rating_diffs = matchup_ratings[:, 1] - matchup_ratings[:, 0]
-    # probs = jax.nn.sigmoid(neg_rating_diffs)
-    probs = logistic.cdf(neg_rating_diffs)
-    return -jnp.log((outcomes * probs) + ((1.0 - outcomes) * (1.0 - probs))).sum()
-
-
-@jax.jit
-def cauchy_predict(ratings, matchups):
-    matchup_ratings = ratings[matchups]
-    rating_diffs = matchup_ratings[:, 0] - matchup_ratings[:, 1]
-    # probs = (1.0 / jnp.pi) * jnp.arctan(rating_diffs) + 0.5
-    probs = cauchy.cdf(rating_diffs)
-    return probs
-
-@jax.jit
-def cauchy_likelihood(ratings, matchups, outcomes):
-    matchup_ratings = ratings[matchups]
-    rating_diffs = matchup_ratings[:, 0] - matchup_ratings[:, 1]
-    # probs = (1.0 / jnp.pi) * jnp.arctan(rating_diffs) + 0.5
-    probs = cauchy.cdf(rating_diffs)
-    return -jnp.log((outcomes * probs) + ((1.0 - outcomes) * (1.0 - probs))).sum()
-
-@jax.jit
-def gaussian_predict(ratings, matchups):
-    matchup_ratings = ratings[matchups]
-    rating_diffs = matchup_ratings[:, 0] - matchup_ratings[:, 1]
-    probs = norm.cdf(rating_diffs)
-    return probs
-
-@jax.jit
-def gaussian_likelihood(ratings, matchups, outcomes):
-    matchup_ratings = ratings[matchups]
-    rating_diffs = matchup_ratings[:, 0] - matchup_ratings[:, 1]
-    probs = norm.cdf(rating_diffs)
-    return -jnp.log((outcomes * probs) + ((1.0 - outcomes) * (1.0 - probs))).sum()
-
 
 def main():
     dataset, test_mask = load_dataset('league_of_legends', test_start_date='2023-03-31')
     print(len(dataset), test_mask.sum())
 
-    logistic = AutogradRatingSystem(
+    logistic_rs = AutogradRatingSystem(
         dataset.competitors,
-        predict_fn=logistic_predict,
-        likelihood_fn=logistic_likelihood,
+        cdf=logistic.cdf,
         initial_rating=0.0,
         learning_rate=0.2,
+        scale=1.0,
         update_method='batched'
     )
-    logistic_metrics = evaluate(logistic, dataset, test_mask)
+    logistic_metrics = evaluate(logistic_rs, dataset, test_mask)
     print('logistic')
     print(logistic_metrics)
 
-    cauchy = AutogradRatingSystem(
+    cauchy_rs = AutogradRatingSystem(
         dataset.competitors,
-        predict_fn=cauchy_predict,
-        likelihood_fn=cauchy_likelihood,
+        cdf=cauchy.cdf,
         initial_rating=0.0,
         learning_rate=0.2,
+        scale=1.0,
         update_method='batched'
     )
-    cauchy_metrics = evaluate(cauchy, dataset, test_mask)
+    cauchy_metrics = evaluate(cauchy_rs, dataset, test_mask)
     print('cauchy')
     print(cauchy_metrics)
 
-    gaussian = AutogradRatingSystem(
+    gaussian_rs = AutogradRatingSystem(
         dataset.competitors,
-        predict_fn=gaussian_predict,
-        likelihood_fn=gaussian_likelihood,
+        cdf=norm.cdf,
         initial_rating=0.0,
         learning_rate=0.2,
+        scale=1.0,
         update_method='batched'
     )
-    gaussian_metrics = evaluate(gaussian, dataset, test_mask)
+    gaussian_metrics = evaluate(gaussian_rs, dataset, test_mask)
     print('gaussian')
     print(gaussian_metrics)
 
